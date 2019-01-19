3c9f26c6ad0bfaa3e2f52ca4a6d9b2052b9c138e
Mariam Walaa <31370480+mwalaa@users.noreply.github.com>
2019-01-19 17:05:04 -0500

    Update shiny-app.R

--- shiny-app.R
+++ shiny-app.R
@@ -2,8 +2,6 @@ library(shiny)
 library(leaflet)
 library(shinythemes)
 library(leaflet.extras)
-install.packages("RgoogleMaps")
-library(RgoogleMaps)
 
 r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
@@ -11,38 +9,24 @@ names(r_colors) <- colors()
 ## UI ----------------------------------
 
 ui <- fluidPage(
-  theme = shinytheme("united"),
   navbarPage("nicheRecommender", tabPanel("Map"), tabPanel("Data"), tabPanel("Plots")),
-  leafletOutput("mapPlot"), # leaflet output for plotting points
-  p(), 
-  actionButton("recalc", "New points"), 
-  mainPanel(
-    plotOutput(outputId = "lineplot", height = "300px"),
-    textOutput(outputId = "desc"),
-    tags$a(href = "https://www.google.com/finance/domestic_trends", 
-           "Source: Google Domestic Trends", target = "_blank")), fluidRow(column(3,
-         h4("Tell us where you want to go."),
-         br(),
-         checkboxInput('Location'),
-         checkboxInput('Categories'))))
+  theme = shinytheme("united"),
+  leafletOutput("mymap"),
+  p(),
+  actionButton("recalc", "New points")
+)
 
 ## SERVER ----------------------------------
 
-server <- function(input, output, session){
-  dat <- read_csv('C:\\Users\\Mariam\\Documents\\yelp_dataset\\yelp_dataset~\\coordinates.csv')
-  subsetData <- reactive({
-    new_data <- dat[altitu]
-  })
+server <- function(input, output, session) {
   points <- eventReactive(input$recalc, {cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)}, ignoreNULL = FALSE)
-  
   output$mymap <- renderLeaflet({
-    leaflet(width=400, height=1080) %>% 
+    leaflet() %>%
       addProviderTiles(providers$Stamen.TonerLite, options = providerTileOptions(noWrap = TRUE)) %>%
-      addMarkers(data = points()) %>%
-    addTiles() %>%
-    addFullscreenControl()
-    })}
-    
+      addMarkers(data = points())
+  })
+}
+  
 ## DEPLOY APP ----------------------------------
-
+  
 shinyApp(ui, server, options=list(height=1080))
