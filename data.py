dedf5a6be161a6e489c63a777011fdf282586a79
Stephen Zhao <szstephenzhao88@gmail.com>
2019-01-19 15:08:55 -0500

    Key

--- data.py
+++ data.py
@@ -2,18 +2,16 @@ import sample as yelp
 import pandas as pd
 from key import API_KEY
 
-
 limit = 50 # max 50 per request
 num_requests = 20
 # num_requests = 20 # (to get 20*50 = 1000)
-location = 'toronto'
+location = 'markham'
 
 dataframes = []
 
-
 for i in range(num_requests):
 
-    df = pd.DataFrame(yelp.search(API_KEY, "", location, lim=limit, offset=i*limit)['businesses'],
+    df = pd.DataFrame(yelp.search(API_KEY, "restaurants", location, lim=limit, offset=i*limit)['businesses'],
                       columns=['id', 'alias', 'rating', 'review_count', 'categories'],
                       )
 
@@ -27,11 +25,23 @@ for i in range(num_requests):
 
 # print(yelp.search(API_KEY, "", 'toronto', lim=50))
 
-
 combined_df = pd.concat(dataframes)
-quantile1 = combined_df['review_count'].quantile(0)
-quantile2 = combined_df['review_count'].quantile(0.1)
 
+start_quantile = 0
+quantile_interval = 0.1
+quantile_increment = 0.01
+min_review_threshold = 5
+# What this code does is start with the places with the bottom 10% of reviews
+# Then what it does is it says, if there are just few reviews in general
+# (ie consider Markham vs Toronto)
+# Then maybe we say there needs to be at least 5 reviews
+# And so adjust the quantile increment upwards
+quantile1 = combined_df['review_count'].quantile(start_quantile)
+while quantile1 < min_review_threshold:
+    start_quantile += quantile_increment
+    quantile1 = combined_df['review_count'].quantile(start_quantile)
+
+quantile2 = combined_df['review_count'].quantile(start_quantile + quantile_interval)
 
 out = []
 
@@ -39,17 +49,13 @@ for index, row in combined_df.iterrows():
     if quantile1 < row['review_count'] < quantile2:
         out.append([row['alias'], row['rating'], row['review_count']])
 
-    # TODO FILTER OUT LANDMARKS, only restaurants
-    # TODO actual sort by rating
-    # TODO more precise location (Toronto is big!)
+    # Possible to do? More precise location (Toronto is big!)? But
+    # somewhere like Markham isn't that big
 
     # print(row['alias'], row['rating'])
 
-print(out)
+out.sort(key= lambda x: -float(x[1])) # reverse order, highest rating first
 
-# for alias in combined_df['alias']:
+num_results = 10
 
-
-
-# for count in df['review_count']:
-#     print(count)
+print(out[:num_results])
